f661741ed312d744a6955d80f5ac30a7a7b6bf44
Bruno Rodrigues <52383016+iDelith@users.noreply.github.com>
2019-07-01 22:45:07 -0300

    Added output as comment

--- Python/Curso Em Video/Mundo 1/Exercicios/ex008.py
+++ Python/Curso Em Video/Mundo 1/Exercicios/ex008.py
@@ -52,3 +52,31 @@ print(
     f'''{'-'*10}''' '\n'
     .format(medida, mm, cm, dm, dam, hm, km)
 )
+
+
+
+# OUTPUT
+
+# Digite uma distância em metros: 5
+
+ 
+# Distância de referência: 5.0 em [metros]
+
+
+#       ----------------------------------------------------------------------
+#        km        hm        dam        m        dm        cm        mm
+#        0.005     0.05      0.5        5.0      50.0      500.0     5000.0
+#       ----------------------------------------------------------------------
+
+
+
+# 5.0m podem ser convertidos para as seguintes medidas: 
+
+# ----------
+# 5000 mm 
+# 500 cm 
+# 50 dm 
+# 0.5 dam 
+# 0.05 hm 
+# 0.005 km 
+# ----------
